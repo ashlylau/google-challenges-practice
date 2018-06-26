c869ea0ff7a4bac4ca4c3f9c08338f49dc6b3dda
al5217 <al5217@ic.ac.uk>
2018-06-26 23:00:35 +0100

    debugged solution. still O(N^2) time algorithm.

--- technical-challenge-4/src/Main.java
+++ technical-challenge-4/src/Main.java
@@ -1,24 +1,31 @@
+import javafx.util.Pair;
+
+import java.util.HashMap;
 import java.util.Map;
 
 public class Main {
 
   public static void main(String[] args) {
-    Integer[] array = new Integer[] {2,4,2,1,10,6,10};
+    Integer[] array = new Integer[] {2,4,2,1};
 
+    Map<Integer, Pair> map = new HashMap<>();
     int maxArea = 0;
 
     for (int i = 0; i < array.length; i++) {
-      int currentHeight;
-      int area = currentHeight = array[i];
+      int currentHeight = array[i];
       for (int j = 1; j < array.length - i; j++) {
-        currentHeight = Math.min(currentHeight, array[j]);
-        area = (j + 1) * currentHeight;
-      }
-      if (area > maxArea) {
-        maxArea = area;
+        currentHeight = Math.min(currentHeight, array[j + i]);
+        int area = (j + 1) * currentHeight;
+        if (area > maxArea) {
+          maxArea = area;
+          Pair<Integer, Integer> indices = new Pair(i, j+i);
+          map.put(maxArea, indices);
+        }
       }
     }
     System.out.println(maxArea);
+    System.out.println("(" + map.get(maxArea).getKey() + "," + map.get(maxArea).getValue() + ")");
+    //System.out.println(map.toString());
   }
 
 }
